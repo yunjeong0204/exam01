767fa237375c25645f7dc25a20123ccdd39fdabd
yunjeong0204 <dua0204@naver.com>
2023-04-24 19:00:38 +0900

    travel

--- js/travel.js
+++ js/travel.js
@@ -8,3 +8,16 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show') 
     .addTo(new ScrollMagic.Controller()); 
 });
+
+
+// 수평슬라이드
+new Swiper('.banner .swiper', {
+  direction : 'horizontal',
+  loop: true, // 반복 재생
+  autoplay: true, 
+  slidePerView: 1,
+  navigation: {
+    prevEl: '.banner .prev',
+    nextEl: '.banner .next'
+  },
+});
