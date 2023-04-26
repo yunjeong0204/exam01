c116cf3d39c0915caa4949168d52a58e3cc82550
yunjeong0204 <dua0204@naver.com>
2023-04-26 19:01:48 +0900

    travel

--- js/travel.js
+++ js/travel.js
@@ -1,17 +1,37 @@
-
 // 수평슬라이드
 new Swiper('.banner .swiper', {
-  direction : 'horizontal',
+  // direction : 'horizontal',
   loop: true, // 반복 재생
   autoplay: {
     delay: 5000
-  }, 
-  slidePerView: 4,
+  },
+  slidesPerView: 4,
   navigation: {
-    prevEl: '.banner .prev',
-    nextEl: '.banner .next'
+    prevEl: '.banner .swiper-button-prev',
+    nextEl: '.banner .swiper-button-next',
   },
 });
+
+// swiper api
+// const swiper = new Swiper('.swiper', {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 2,
+//       spaceBetween: 20
+//     },
+//     480: {
+//       slidesPerView: 3,
+//       spaceBetween: 30
+//     },
+//     640: {
+//       slidesPerView: 4,
+//       spaceBetween: 40
+//     }
+//   }
+// })
 
 const spyEls = document.querySelectorAll('section.scroll-spy');
 spyEls.forEach(function (spyEl) {
