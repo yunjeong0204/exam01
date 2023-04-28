276ee51ed2067843284412b0fbc54931ea814b18
yunjeong0204 <dua0204@naver.com>
2023-04-28 19:01:48 +0900

    travel

--- js/travel.js
+++ js/travel.js
@@ -1,3 +1,21 @@
+// header
+const btnmenuicons = document.querySelector('.menu-icons');
+const btnnav = document.querySelector('.nav');
+const btngroup = document.querySelector('.btngroup');
+
+const mobile = document.querySelector('.mo-nav');
+const btnclose = document.querySelector('.btn-close');
+
+
+btnmenuicons.addEventListener('click', function () {
+  mobile.classList.add('active');
+});
+
+btnclose.addEventListener('click', function () {
+  mobile.classList.remove('active');
+});
+
+
 // 수평슬라이드
 new Swiper('.banner .swiper', {
   // direction : 'horizontal',
@@ -13,26 +31,24 @@ new Swiper('.banner .swiper', {
 });
 
 // swiper api
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 4,
-  // spaceBetween: 5,
-
-  breakpoints: {
-  
-    // 780: {
-    //   slidesPerView: 2,
-    //   spaceBetween: 20
-    // },
-    // 780: {
-    //   slidesPerView: 3,
-    //   spaceBetween: 30
-    // },
-    // 640: {
-    //   slidesPerView: 4,
-    //   spaceBetween: 40
-    // }
-  }
-})
+// const swiper = new Swiper('.swiper', {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   breakpoints: {
+//     480: {
+//       slidesPerView: 2,
+//       spaceBetween: 20
+//     },
+//     640: {
+//       slidesPerView: 3,
+//       spaceBetween: 30
+//     },
+//     780: {
+//       slidesPerView: 4,
+//       spaceBetween: 40
+//     }
+//   }
+// })
 
 const spyEls = document.querySelectorAll('section.scroll-spy');
 spyEls.forEach(function (spyEl) {
